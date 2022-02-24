7910e9233d3b165db7568831cc798646c1210323
ShallowFeather <74778927+ShallowFeather@users.noreply.github.com>
2022-02-24 23:02:10 +0800

    init

---
+++ Lab3/Code/Run/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sf/文件/圖學/Lab3/Code/Texture.cpp" "/home/sf/文件/圖學/Lab3/Code/Run/CMakeFiles/Rasterizer.dir/Texture.cpp.o"
+  "/home/sf/文件/圖學/Lab3/Code/Triangle.cpp" "/home/sf/文件/圖學/Lab3/Code/Run/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "/home/sf/文件/圖學/Lab3/Code/main.cpp" "/home/sf/文件/圖學/Lab3/Code/Run/CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "/home/sf/文件/圖學/Lab3/Code/rasterizer.cpp" "/home/sf/文件/圖學/Lab3/Code/Run/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  ".././include"
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Lab3/Code/cmake-build-debug/CMakeFiles/Rasterizer.dir/Texture.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/Rasterizer.dir/Texture.cpp.o: \
+ /home/sf/文件/圖學/Lab3/Code/Texture.cpp /usr/include/stdc-predef.h
